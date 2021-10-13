c917a3f25738d61d48ce92478c7ef4ef7511d0f9
Justin Wong <justin.wong235@gmail.com>
2021-10-13 17:35:40 -0400

    catchup=False

--- pipeline/dags/covid_dag.py
+++ pipeline/dags/covid_dag.py
@@ -10,7 +10,6 @@ import json
 from scripts.covid_pandas import main
 from helpers import validation_helper
 
-
 json_path = './dags/config/aws_config.json'
 with open(json_path) as file:
     env = json.load(file)
@@ -18,15 +17,19 @@ with open(json_path) as file:
 default_args = {
     'owner': 'jwong',
     'depends_on_past': False,
-    'start_date': datetime(2021, 5, 5),
-    'catchup': False,
+    'start_date': datetime(2021, 8, 3),
     'email': ['justin.wong235@gmail.com'],
     'email_on_failure': True,
     'email_on_retry': False,
     'retries': 0,
     'retry_delay': timedelta(minutes=1),
 }
-dag = DAG('covid1', default_args=default_args, schedule_interval='0 13 * * *')
+dag = DAG(
+    'covid1',
+    default_args=default_args,
+    catchup=False,
+    schedule_interval='0 12 * * *'
+)
 
 
 def create_conn(conn_id, conn_type, host, schema, login, password, port):
@@ -42,8 +45,10 @@ def create_conn(conn_id, conn_type, host, schema, login, password, port):
         password=password,
         port=port
     )
-    session = settings.Session() # get the session
-    conn_name = session.query(Connection).filter(Connection.conn_id == conn.conn_id).first()
+    session = settings.Session()  # get the session
+    conn_name = session.query(Connection).filter(
+        Connection.conn_id == conn.conn_id
+    ).first()
 
     if str(conn_name) == str(conn_id):
         print(f"Connection {conn_id} already exists")
@@ -56,7 +61,15 @@ create_rds_conn = PythonOperator(
     dag=dag,
     task_id='create_rds_conn',
     python_callable=create_conn,
-    op_kwargs={'conn_id': 'rds', 'conn_type': env['RDS']['TYPE'], 'host': env['RDS']['HOST'], 'schema': env['RDS']['SCHEMA'], 'login': env['RDS']['LOGIN'], 'password': env['RDS']['PASSWORD'], 'port': env['RDS']['PORT']}
+    op_kwargs={
+        'conn_id': 'rds',
+        'conn_type': env['RDS']['TYPE'],
+        'host': env['RDS']['HOST'],
+        'schema': env['RDS']['SCHEMA'],
+        'login': env['RDS']['LOGIN'],
+        'password': env['RDS']['PASSWORD'],
+        'port': env['RDS']['PORT']
+    }
 )
 
 #pandas script that moves data in github to S3 csv file
@@ -64,10 +77,11 @@ load_to_s3 = PythonOperator(
     task_id='data_to_s3',
     dag=dag,
     python_callable=main,
-    op_args=[env['AWS']['LOGIN'], env['AWS']['PW'], 's3://' + env['S3']['BUCKET'] + '/']
+    op_args=[
+        env['AWS']['LOGIN'], env['AWS']['PW'],
+        's3://' + env['S3']['BUCKET'] + '/'
+    ]
 )
-
-
 '''
 drop_existing = PostgresOperator(
     task_id='drop_existing',
@@ -90,7 +104,6 @@ create_tables = PostgresOperator(
     postgres_conn_id='rds',
     sql='/sql/02_create_tables.sql'
 )
-
 
 stage_tables = PostgresOperator(
     task_id='stage_tables',
@@ -134,21 +147,29 @@ validate_fact_confirmed = PythonOperator(
     task_id='validate_fact_confirmed',
     dag=dag,
     python_callable=validation_helper.validate_fact_metric,
-    op_args=[['staging.staging_global_confirmed', 'staging.staging_us_confirmed'], 'fact.fact_metrics', 'confirmed']
+    op_args=[
+        ['staging.staging_global_confirmed', 'staging.staging_us_confirmed'],
+        'fact.fact_metrics', 'confirmed'
+    ]
 )
 
 validate_fact_deaths = PythonOperator(
     task_id='validate_fact_deaths',
     dag=dag,
     python_callable=validation_helper.validate_fact_metric,
-    op_args=[['staging.staging_global_deaths', 'staging.staging_us_deaths'], 'fact.fact_metrics', 'deaths']
+    op_args=[
+        ['staging.staging_global_deaths', 'staging.staging_us_deaths'],
+        'fact.fact_metrics', 'deaths'
+    ]
 )
 
 validate_fact_recovered = PythonOperator(
     task_id='validate_fact_recovered',
     dag=dag,
     python_callable=validation_helper.validate_fact_metric,
-    op_args=[['staging.staging_global_recovered'], 'fact.fact_metrics', 'recovered']
+    op_args=[
+        ['staging.staging_global_recovered'], 'fact.fact_metrics', 'recovered'
+    ]
 )
 
 #population thresh is based off of median (50th percentile) populations for respective location level
@@ -170,5 +191,6 @@ validate_bi_counts = PythonOperator(
     python_callable=validation_helper.validate_bi_counts
 )
 
-
-load_to_s3 >> create_rds_conn >> drop_staging >> create_tables >> stage_tables >> load_dim_tables >> load_temp_fact_tables >> load_fact_tables >> [validate_fact_confirmed, validate_fact_deaths, validate_fact_recovered] >> load_bi_tables >> validate_bi_counts
+load_to_s3 >> create_rds_conn >> drop_staging >> create_tables >> stage_tables >> load_dim_tables >> load_temp_fact_tables >> load_fact_tables >> [
+    validate_fact_confirmed, validate_fact_deaths, validate_fact_recovered
+] >> load_bi_tables >> validate_bi_counts
